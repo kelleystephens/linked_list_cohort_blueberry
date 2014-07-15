4d931f5757d957d59aaab7b92734b174815128bd
Kelley Stephens <kelley.stephens@outlook.com>
2014-07-15 11:06:10 -0500

    linked lists completed

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,8 +1,13 @@
 class LinkedList
 
+  attr_reader :size
+  # shorter way to write:
+  # def size
+  #   @size
+  # end
+
   def initialize(*payloads)
     @size = 0
-    @contents = []
     payloads.each do | item_payload |
       push(item_payload)
     end
@@ -16,23 +21,11 @@ class LinkedList
     else
       @first_item = lli
     end
-    @contents << lli.payload
   end
 
   def get(index)
-    # raise IndexError if index < 0
-    # current_item = @first_item
-    # index.times do
-    #   raise IndexError if current_item == nil
-    #   current_item = current_item.next_item
-    # end
-    # current_item.payload
     item = find_by_index(index)
     item.payload
-  end
-
-  def size
-    @size
   end
 
   def last
@@ -40,11 +33,17 @@ class LinkedList
   end
 
   def to_s
-    if @size == 0
-      "| |"
-    else
-      "| #{@contents.join(', ')} |"
+    result = "|"
+    current_item = @first_item
+    until current_item.nil?
+      result << " #{current_item.payload}"
+      unless current_item.last?
+        result << ","
+      end
+      current_item = current_item.next_item
     end
+    result << " |"
+    result
   end
 
   def find_by_index (index)
@@ -64,17 +63,72 @@ class LinkedList
   def []= (index, value)
     item = find_by_index(index)
     item.payload = value
-    @contents[index] = value
   end
 
   def delete (index)
-    item = find_by_index(index)
     @size -= 1
-    @contents.delete_at(index)
+    if index == 0
+      @first_item = find_by_index(1)
+    else
+      # deleted item = find_by_index(index), just remove it from being pointed at or pointing to another
+      prev = find_by_index(index - 1)
+      new_next = find_by_index(index + 1)
+      prev.next_item = new_next
+    end
   end
 
   def index (payload)
-    @contents.find_index(payload)
+    index = -1
+    current_item = @first_item
+    until current_item.nil?
+      index += 1
+      return index if current_item.payload == payload
+      current_item = current_item.next_item
+    end
+  end
+
+  def sorted?
+    current_item = @first_item
+    if @size <= 1
+      true
+    else
+      until current_item.last?
+        return false if current_item > current_item.next_item
+        current_item = current_item.next_item
+      end
+      true
+    end
+  end
+
+  def sort!
+    current_item = @first_item
+    until sorted?
+      result = current_item <=> current_item.next_item
+      if result == 1
+        index = index(current_item.payload)
+        swap_with_next(index)
+        sort!
+      else
+        current_item = current_item.next_item
+      end
+    end
+    self.to_s
+  end
+
+  def swap_with_next(index)
+    swap_item_1 = find_by_index(index)
+    swap_item_2 = find_by_index(index + 1)
+    right_item = find_by_index(index + 2)
+    if index > 0
+      left_item = find_by_index(index - 1)
+      left_item.next_item = swap_item_2
+      swap_item_2.next_item = swap_item_1
+      swap_item_1.next_item = right_item
+    else
+      @first_item = swap_item_2
+      swap_item_2.next_item = swap_item_1
+      swap_item_1.next_item = right_item
+    end
   end
 
   private
